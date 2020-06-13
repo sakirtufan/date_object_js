1f04df9ab40f6564405ccf42d67e0c0062f65508
sakir tufan <sakirtufan49@gmail.com>
2020-06-13 15:07:45 +0200

    first commit

--- script.js
+++ script.js
@@ -1,43 +1,85 @@
-// YAS HESAPLAMASI
+let aracBilgileri = [
+  {
+    id: 'bmw116d_1234',
+    model : '116d',
+    yil : 2015,
+    renk : 'white',
+    servisKayitlari : [
+      {
+        id : 'bmw116d_1234_1', tarih : '30.01.2016', km:13000, toplamÜcret:900,
+        servisDetayi : 
+        [
+          {id : 'bmw116d_1234_1_1', islem : 'yag degisimi', ücret : 300},
+          {id : 'bmw116d_1234_1_2', islem : 'filtre degisimi', ücret : 300},
+          {id : 'bmw116d_1234_1_3', islem : 'fren hidroligi', ücret : 300}      
+        ]
+      },
+      {
+        id : 'bmw116d_1234_2', tarih : '10.01.2017', km:28000, toplamÜcret:1800,
+        servisDetayi : 
+        [
+          {id : 'bmw116d_1234_2_1', islem : 'yag degisimi', ücret : 350},
+          {id : 'bmw116d_1234_2_2', islem : 'filtre degisimi', ücret : 350},
+          {id : 'bmw116d_1234_2_3', islem : 'fren hidroligi', ücret : 300},
+          {id : 'bmw116d_1234_2_4', islem : 'balata degisimi', ücret : 800}      
+        ]
+      }
+    ]
+  },
+  {
+    id: 'bmw118i_1234',
+    model : '118i',
+    yil : 2015,
+    renk : 'white',
+    servisKayitlari : [
+      {
+        id : 'bmw1118i_1234_1', tarih : '30.01.2016', km:13000, toplamÜcret:900,
+        servisDetayi : 
+        [
+          {id : 'bmw118i_1234_1_1', islem : 'yag degisimi', ücret : 300},
+          {id : 'bmw118i_1234_1_2', islem : 'filtre degisimi', ücret : 300},
+          {id : 'bmw118i_1234_1_3', islem : 'fren hidroligi', ücret : 300}      
+        ]
+      },
+      {
+        id : 'bmw118i_1234_2', tarih : '10.01.2017', km:28000, toplamÜcret:1800,
+        servisDetayi : 
+        [
+          {id : 'bmw118i_1234_2_1', islem : 'yag degisimi', ücret : 350},
+          {id : 'bmw118i_1234_2_2', islem : 'filtre degisimi', ücret : 350},
+          {id : 'bmw118i_1234_2_3', islem : 'fren hidroligi', ücret : 300},
+          {id : 'bmw118i_1234_2_4', islem : 'balata degisimi', ücret : 800}      
+        ]
+      }
+    ]
+  },
+  {
+    id: 'bmw320d_1234',
+    model : '320d',
+    yil : 2015,
+    renk : 'white',
+    servisKayitlari : [
+      {
+        id : 'bmw320d_1234_1', tarih : '30.01.2016', km:13000, toplamÜcret:900,
+        servisDetayi : 
+        [
+          {id : 'bmw320d_1234_1_1', islem : 'yag degisimi', ücret : 300},
+          {id : 'bmw320d_1234_1_2', islem : 'filtre degisimi', ücret : 300},
+          {id : 'bmw320d_1234_1_3', islem : 'fren hidroligi', ücret : 300}      
+        ]
+      },
+      {
+        id : 'bmw320d_1234_2', tarih : '10.01.2017', km:28000, toplamÜcret:1800,
+        servisDetayi : 
+        [
+          {id : 'bmw320d_1234_2_1', islem : 'yag degisimi', ücret : 350},
+          {id : 'bmw320d_1234_2_2', islem : 'filtre degisimi', ücret : 350},
+          {id : 'bmw320d_1234_2_3', islem : 'fren hidroligi', ücret : 300},
+          {id : 'bmw320d_1234_2_4', islem : 'balata degisimi', ücret : 800}      
+        ]
+      }
+    ]
+  }
+]
 
-var birthday = new Date('10/04/1984')
-
-var ageDifMs = Date.now() - birthday.getTime();
-
-console.log(ageDifMs); //milisecond cinsinden yas
-
-var ageDate = new Date(ageDifMs);
-
-console.log(ageDate);
-
-console.log(ageDate.getFullYear() - 1970);
-
-/*
-console.log(Date.now());
-
-console.log(birthday.getTime()); milisecond cinsinden yazar
-
-console.log(Date.now());  Date.now metodu 1970 den simdiki tarihe kadar gecen süreyi milisecond cinsinden gösterir.
-
-*/
-
-
-
-// Her yil mayis ayinin 2.haftasi pazar günü kutlanan anneler günü 2021 yilinda ne zaman kutlanacaktir ?
-
-var annelerGünü = new Date();
-annelerGünü.setHours(0,0,0,0);
-annelerGünü.setFullYear(2021);
-annelerGünü.setDate(1);
-annelerGünü.setMonth(4);
-
-while(annelerGünü.getDay() != 0){
-  annelerGünü.setDate(annelerGünü.getDate()+1)
-}
-
-annelerGünü.setDate(annelerGünü.getDate()+7);
-console.log('2021 yilinda anneler günü:'+annelerGünü);
-
-
-
-
+console.log(aracBilgileri[0].servisKayitlari[0].servisDetayi);
